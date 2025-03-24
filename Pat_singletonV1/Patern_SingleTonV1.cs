2c6a884da0b8655d7e3ba8de40572195f21ece78
Pascal100500 <kingandbuffon@gmail.com>
2025-03-24 21:02:33 +0300

    Добавьте файлы проекта.

---
+++ Pat_singletonV1/Patern_SingleTonV1.cs
@@ -0,0 +1,33 @@
+﻿// Pattern Singleton Variant 1
+
+//Patern_SingleTonV1.cs - для копирования названия
+using System;
+
+class Logger
+{
+    private static Logger _instance;
+       private Logger()
+
+    {
+        // В будущем можно открыть файл для логов или настроить форматирование
+    }
+
+    //Метод "ленивого создания", экземпляр создается только тогда, когда он впервые понадобится
+    public static Logger Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new Logger();
+            }
+            return _instance;
+        }
+    }
+
+    //Метод для вывода сообщений
+    public void Log(string message)
+    {
+        Console.WriteLine($"[LOG] {message}");
+    }
+}
---
+++ Pat_singletonV1/Programm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Patern_SingleTonV1.cs;
+
+class Program
+{
+    static void Main()
+{
+    Logger logger1 = Logger.Instance;
+    logger1.Log("Первое сообщение");
+
+    Logger logger2 = Logger.Instance;
+    logger2.Log("Второе сообщение");
+
+    Console.WriteLine(ReferenceEquals(logger1, logger2)); 
+}
+}
